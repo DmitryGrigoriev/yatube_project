841d211838222eac030a0c32b87012a2a6b06564
Dmitry Grigorev <grigd2013@gmail.com>
2023-02-24 23:11:47 +0100

    Add var to template

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -7,11 +7,22 @@ def index(request):
     """Функция для отображения сообщения на главной странице."""
 
     template = 'posts/index.html'
-    return render(request, template)
+    text = 'Это главная страница проекта Yatube'
+
+    context = {
+        'text': text
+    }
+
+    return render(request, template, context)
 
 
 def group_post(request, slug):
     """Функция для отображения сообщения для страницы группы."""
 
     template = 'posts/group_list.html'
-    return render(request, template)
+    text = 'Здесь будет информация о группах проекта Yatube'
+
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
