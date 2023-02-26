438775ae30a3ad521b84c2ac05bfb3e45b02987e
Dmitry Grigorev <grigd2013@gmail.com>
2023-02-26 11:15:50 +0100

    Add filter by group

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 # Create your views here.
 
 
 def index(request):
-    """Функция для отображения сообщения на главной странице."""
+    """Функция для выведения 10 последних записей."""
 
     posts = Post.objects.order_by('-pub_date')[:10]
 
@@ -19,10 +19,11 @@ def index(request):
 def group_post(request, slug):
     """Функция для отображения сообщения для страницы группы."""
 
-    template = 'posts/group_list.html'
-    text = 'Здесь будет информация о группах проекта Yatube'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.object.filter(group=group).order_by('-pub_date')[:10]
 
     context = {
-        'text': text
+        'group': group,
+        'posts': posts
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
