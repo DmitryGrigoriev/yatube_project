ae305735b85239c77e95e135165821c42e2369e8
Dmitry Grigorev <grigd2013@gmail.com>
2023-02-27 22:20:25 +0100

    Passed tests

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -11,7 +11,7 @@ class PostAdmin(admin.ModelAdmin):
                     'author',
                     'group')
     list_editable = ('group',)
-    search_field = ('text',)
+    search_fields = ('text',)
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
 
